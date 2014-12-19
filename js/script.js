690c64eda58f6439fb811c35d3b3602e98a06a6f
andela-amaborukoje <adebayo@andela.co>
2014-12-19 18:58:09 +0100

    fixed the scope variable

--- js/script.js
+++ js/script.js
@@ -1,43 +1,41 @@
 var weatherApp = angular.module('weatherApp', []);
 
-//weatherApp.controller('weatherController', function($scope, weatherAppFactory){
 weatherApp.controller('weatherController', function($scope, weatherAppFactory, googleMapDisplay){  
-    $scope.results= [];
-    
-    // $scope.mapCanvas = [];
-    $scope.userSearch = "";
-    //$scope.mapCanvas = " "; check this out 
-    $scope.displayResult = function(){
-      if($scope.userSearch === ""){
-        console.log('got here');
-        $scope.results= [];
-        $scope.mapStatus = false;
-        $scope.statusDisplay = "PLEASE ENTER A NAME OF CITY OR STATE";
+    var user = $scope;
+    user.results= [];
+    user.userSearch = "";
+
+    user.displayResult = function(){
+      if(user.userSearch === ""){
+        // console.log('got here');
+        user.results= [];
+        user.mapStatus = false;
+        user.statusDisplay = "PLEASE ENTER A NAME OF CITY OR STATE";
       } 
       else {
-        weatherAppFactory.getUrl($scope.userSearch).
+        weatherAppFactory.getUrl(user.userSearch).
         success( function (data, status) {
           if (data.cod === 200){
-            $scope.results = [];
-            $scope.statusDisplay = "Displaying Result for ";
-            $scope.mapStatus = true;
-            $scope.results.push(data);
+            user.results = [];
+            user.statusDisplay = "Displaying Result for ";
+            user.mapStatus = true;
+            user.results.push(data);
 
             googleMapDisplay.getMap(data.coord.lat, data.coord.lon);
           }else{
             //this is the error report generated when the query returns no value
             console.log(data.cod);
-            $scope.mapStatus = false;
-            $scope.statusDisplay = data.message;
+            user.mapStatus = false;
+            user.statusDisplay = data.message;
 
-            $scope.results= [];
+            user.results= [];
           }
         }) //end of the success promise
         .error( function (data, status) {
             //console log the error for not internet connection here
-          $scope.mapStatus = false;
-          $scope.results = [];   
-          $scope.statusDisplay = " Cannot Connect to the Server, Check your Internet Connection and try again " ;
+          user.mapStatus = false;
+          user.results = [];   
+          user.statusDisplay = " Cannot Connect to the Server, Check your Internet Connection and try again " ;
 
         });//end of the error  promise
       } // end of else statement when query is not empty
