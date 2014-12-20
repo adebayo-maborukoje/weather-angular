881d0bc32e680a5bd9d6683e8340b1bbb9522552
andela-amaborukoje <adebayo@andela.co>
2014-12-20 23:07:18 +0100

    fixed the loading icon button and flickering images

--- js/script.js
+++ js/script.js
@@ -1,42 +1,47 @@
 var weatherApp = angular.module('weatherApp', []);
 
-weatherApp.controller('weatherController', function($scope, weatherAppFactory, googleMapDisplay){  
+weatherApp.controller('weatherController', function($scope, weatherAppFactory,btnEffect, googleMapDisplay){  
     var app = $scope;
     app.results= [];
     app.userSearch = "";
 
     app.displayResult = function(){
       if(app.userSearch === ""){
-         app.results= [];
+        btnEffect.getStatus(true);
+        app.results= [];
         app.mapStatus = false;
         app.statusDisplay = "PLEASE ENTER A NAME OF CITY OR STATE";
+        btnEffect.getStatus(false);
       } 
       else {
+        btnEffect.getStatus(true);
         weatherAppFactory.getUrl(app.userSearch).
         success( function (data, status) {
+
           if (data.cod === 200){
             app.results = [];
             app.statusDisplay = "Displaying Result for ";
             app.mapStatus = true;
-            
+            app.results.push(data);
                // method 1  //
 
             var temp = parseInt(data.main.temp, 10);
                 temp = Math.round(temp-273.15);
-           //     results.main.temp = temp;
-                app.results.push(data);
-                app.results.main.temp.push(temp);
+               results.main.temp = temp; 
+           // confirm if the  =  or the push works
+                
+            //    app.results.main.temp.push(temp);
          
               // End of method 1 //
 
               // Method 2  for changing the temperature value//
-            app.$watch('results.main.temp', function (newValue, oldValue){
+      /*      app.$watch('results.main.temp', function (newValue, oldValue){
               if(newValue){
                 var temp = parseInt(newValue, 10);
                 temp = Math.round(temp-273.15);
               results.main.temp = temp;
               }
-            });
+            }); */
                 //End of Method 2 //
 
             googleMapDisplay.getMap(data.coord.lat, data.coord.lon);
@@ -50,38 +55,16 @@ weatherApp.controller('weatherController', function($scope, weatherAppFactory, g
           }
         }) //end of the success promise
         .error( function (data, status) {
-            //console log the error for not internet connection here
+            //console log the error for non 200 reports internet connection here
           app.mapStatus = false;
           app.results = [];   
           app.statusDisplay = " Cannot Connect to the Server, Check your Internet Connection and try again " ;
 
-        });//end of the error  promise
-      } // end of else statement when query is not empty
-      
-    } // end of displayResult function
-      
-/*
- do the animation for the searching bar and css styling  learn about ngAnimate
- btnEffect : function() {
-//                 $('#submit').prop('disabled', false);
-//                 $('#submit').val("Forecast").css('background-image', 'none');
-*/
-}); // end of controller
-
-/* Method 3 for changing the temperature  */
-weatherApp.factory('tempCvtFactory', function(){
-    var tempConverter = {};
-    tempConverter.convert = function(tempInKelvin){
-        console.log(tempInKelvin);
-
-        var temp = parseInt(tempInKelvin, 10);
-        temp = Math.round(tempInKelvin - 273.15);
-        console.log(temp);
-        return temp;
-    };
-    return tempConverter;
-});
- /* End of  Method 3*/
+        });
+        btnEffect.getStatus(false);
+      } 
+    }; 
+}); 
 
 weatherApp.factory('weatherAppFactory', ['$http',  function($http) {
      var requestParameters = {};
@@ -98,17 +81,32 @@ weatherApp.factory ('googleMapDisplay', function () {
   var mapObj ={};
 
   mapObj.getMap = function(latitude, longitude) {
-                // console.log(longitude +" and "+ latitude);
-                var mapOptions = {    
-                      center: {
-                        lat: latitude, 
-                        lng: longitude
-                        },
-                        zoom: 7
-                      };
+      var mapOptions = {    
+            center: { lat: latitude, 
+                    lng: longitude
+                  },
+            zoom: 7
+          };
       var map = new google.maps.Map(document.getElementById('mapCanvas'), mapOptions);
       };
-     return mapObj;
+    return mapObj;
 }); //end of googleMapDisplay factory
 
-//
+weatherApp.factory ('btnEffect', function () {
+  return {
+    getStatus : function (BtnStatus) { 
+      if(BtnStatus){
+        $('#submit').prop('disabled', true);
+        $('#submit').val(" ").css('background', 'url("./images/loading2.gif") no-repeat white center');
+      }
+      else {
+        $('#submit').prop('disabled', false);
+        $('#submit').val("Forecast").css('background-image', 'none');
+      }
+      return;
+    }
+  };
+});
+
+
+//write a service for the images that are displayed in respect to the temp
